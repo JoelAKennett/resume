ab1afd04a00ad15724ccee9fa5469c822136fbb3
Joel Kennett <Joel.Kennett185@gmail.com>
2019-06-18 21:56:43 +1000

    Adding basic create react app scafolding and testing out styled-components

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Fragment } from 'react';
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
+import logo from './logo.svg';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`
+
+const StyledAppContainer = styled.div`
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const StyledLink = styled.a`
+  color: #61dafb;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const ReactLogo = styled.img`
+  animation: ${rotate} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <StyledAppContainer>
+        <StyledHeader>
+          <ReactLogo src={logo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <StyledLink
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </StyledLink>
+        </StyledHeader>
+      </StyledAppContainer>
+    </Fragment>
+  );
+}
+
+export default App;
