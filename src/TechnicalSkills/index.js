465d65516b0a451dc46122e1d5ea4440934171c6
Joel Kennett <Joel.Kennett185@gmail.com>
2019-06-24 23:30:37 +1000

    Adding buttons and styling to technology list

--- src/TechnicalSkills/index.js
+++ src/TechnicalSkills/index.js
@@ -18,15 +18,40 @@ const SkillList = styled.ul`
   list-style-type: none;
 `;
 
+const SkillButton = styled.button`
+  font-size: 100%;
+  font-family: inherit;
+  background-color: inherit;
+  color: inherit;
+  padding: 1rem 2rem;
+  border: none;
+  text-decoration: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  &:hover, &:focus {
+    outline: 1px solid #fff;
+    outline-offset: -4px;
+  }
+
+  &:focus {
+    background-color: #010400;
+  }
+
+  &:active {
+    transform: scale(0.97);
+  }
+`;
+
 const TechnicalSkills = () => (
   <Fragment>
     <StyledHeading>Technology Stack:</StyledHeading>
     <SkillList>
-      <li>React</li>
-      <li>Node.js</li>
-      <li>Docker</li>
-      <li>AWS</li>
-      <li>MongoDB</li>
+      <li><SkillButton type="button">React</SkillButton></li>
+      <li><SkillButton type="button">Node.js</SkillButton></li>
+      <li><SkillButton type="button">Docker</SkillButton></li>
+      <li><SkillButton type="button">AWS</SkillButton></li>
+      <li><SkillButton type="button">MongoDB</SkillButton></li>
     </SkillList>
   </Fragment>
 );
